bb998188353e8d5e544540b536a776f708a50de2
Matteo Collina <hello@matteocollina.com>
2015-08-04 19:50:05 +0200

    Fully support for readable, writable, duplexes and trasforms.

--- tentacoli.js
+++ tentacoli.js
@@ -10,6 +10,7 @@ var uuid = require('uuid')
 var nos = require('net-object-stream')
 var callbackStream = require('callback-stream')
 var copy = require('shallow-copy')
+var pump = require('pump')
 var UUIDregexp = /[^-]{8}-[^-]{4}-[^-]{4}-[^-]{4}-[^-]{12}/
 
 function Tentacoli (opts) {
@@ -50,7 +51,7 @@ function Tentacoli (opts) {
         }
       }
 
-      dataStream.pipe(callbackStream({ objectMode: true }, function (err, list) {
+      dataStream.pipe(callbackStream.obj(function (err, list) {
         if (err) {
           that.emit('requestError', err)
           response.ack.error = err.message
@@ -89,22 +90,65 @@ function Tentacoli (opts) {
 }
 
 function mapStream (key) {
+  var stream = this[key]
+  var objectMode = false
+  var type
+
+  if (!stream._transform && stream._readableState && stream._writableState) {
+    type = messages.StreamType.Duplex
+    objectMode = stream._readableState.objectMode || stream._writableState.objectMode
+  } else if (!stream._writableState || stream._readableState && stream._readableState.pipesCount === 0) {
+    type = messages.StreamType.Readable
+    objectMode = stream._readableState.objectMode
+  } else {
+    type = messages.StreamType.Writable
+    objectMode = stream._writableState.objectMode
+  }
+
   // this is the streams$ object
   return {
     id: uuid.v4(),
     name: key,
-    objectMode: this[key]._readableState.objectMode,
-    stream: this[key]
+    objectMode: objectMode,
+    stream: stream,
+    type: type
   }
 }
 
 function pipeStream (container) {
   // this is the tentacoli instance
   var dest = this.createStream(container.id)
-  if (container.objectMode) {
-    dest = nos(dest)
+
+  if (container.type === messages.StreamType.Readable ||
+      container.type === messages.StreamType.Duplex) {
+
+    if (container.objectMode) {
+      pump(
+        container.stream,
+        nos.encoder(),
+        dest)
+    } else {
+      pump(
+        container.stream,
+        dest)
+    }
   }
-  container.stream.pipe(dest)
+
+  if (container.type === messages.StreamType.Writable ||
+      container.type === messages.StreamType.Duplex) {
+
+    if (container.objectMode) {
+      pump(
+        dest,
+        nos.decoder(),
+        container.stream)
+    } else {
+      pump(
+        dest,
+        container.stream)
+    }
+  }
+
   return container
 }
 
@@ -117,6 +161,8 @@ function waitingOrReceived (that, id) {
   } else {
     stream = that.receiveStream(id, { halfOpen: true })
   }
+
+  stream.halfOpen = true
 
   return stream
 }
@@ -160,7 +206,7 @@ Tentacoli.prototype.request = function (msg, callback) {
   stream.on('finish', function () {
     req.stream = nos(that.createStream(req.id, { halfOpen: true }), that._opts)
 
-    req.stream.pipe(callbackStream({ objectMode: true }, function (err, list) {
+    req.stream.pipe(callbackStream.obj(function (err, list) {
       if (err) {
         // TODO cleanup other stream?
         return callback(err)
@@ -183,8 +229,19 @@ Tentacoli.prototype.request = function (msg, callback) {
     if (decoded.streams.length > 0) {
       result[0].streams$ = decoded.streams.reduce(function (acc, container) {
         var stream = waitingOrReceived(that, container.id)
+        var writable
         if (container.objectMode) {
-          stream = nos(stream)
+          if (container.type === messages.StreamType.Duplex) {
+            stream = nos(stream)
+          } else if (container.type === messages.StreamType.Readable) {
+            // if it is a readble, we close this side
+            stream.end()
+            stream = pump(stream, nos.decoder())
+          } else if (container.type === messages.StreamType.Writable) {
+            writable = nos.encoder()
+            pump(writable, stream)
+            stream = writable
+          }
         }
         acc[container.name] = stream
         return acc
--- test.js
+++ test.js
@@ -4,6 +4,8 @@ var test = require('tape')
 var tentacoli = require('./')
 var from = require('from2')
 var callback = require('callback-stream')
+var Writable = require('stream').Writable
+var through = require('through2')
 
 test('can issue a request', function (t) {
   t.plan(3)
@@ -58,7 +60,7 @@ test('can pass through object readable streams', function (t) {
 
   sender.request(msg, function (err, res) {
     t.error(err, 'no error')
-    res.streams$.result.pipe(callback({ objectMode: true }, function (err, list) {
+    res.streams$.result.pipe(callback.obj(function (err, list) {
       t.error(err, 'no error')
       t.deepEqual(list, ['hello', 'streams'], 'is passed through correctly')
     }))
@@ -68,6 +70,94 @@ test('can pass through object readable streams', function (t) {
     reply(null, {
       streams$: {
         result: from.obj(['hello', 'streams'])
+      }
+    })
+  })
+})
+
+test('can pass through object writable streams', function (t) {
+  t.plan(2)
+
+  var sender = tentacoli()
+  var receiver = tentacoli()
+  var msg = { cmd: 'publish' }
+
+  sender.pipe(receiver).pipe(sender)
+
+  sender.request(null, function (err, res) {
+    t.error(err, 'no error')
+    res.streams$.writable.end(msg)
+  })
+
+  receiver.on('request', function (req, reply) {
+    var writable = new Writable({ objectMode: true })
+    writable._write = function (chunk, enc, cb) {
+      t.deepEqual(chunk, msg, 'msg match')
+      cb()
+    }
+    reply(null, {
+      streams$: {
+        writable: writable
+      }
+    })
+  })
+})
+
+test('can pass through a transform stream as a writable', function (t) {
+  t.plan(2)
+
+  var sender = tentacoli()
+  var receiver = tentacoli()
+  var msg = { cmd: 'publish' }
+
+  sender.pipe(receiver).pipe(sender)
+
+  sender.request(null, function (err, res) {
+    t.error(err, 'no error')
+    res.streams$.writable.end(msg)
+  })
+
+  receiver.on('request', function (req, reply) {
+    var writable = new Writable({ objectMode: true })
+    writable._write = function (chunk, enc, cb) {
+      t.deepEqual(chunk, msg, 'msg match')
+    }
+    var transform = through.obj()
+
+    transform.pipe(writable)
+
+    reply(null, {
+      streams$: {
+        writable: transform
+      }
+    })
+  })
+})
+
+test('can pass through a transform stream as a readable streams', function (t) {
+  t.plan(3)
+
+  var sender = tentacoli()
+  var receiver = tentacoli()
+  var msg = { cmd: 'subscribe' }
+
+  sender.pipe(receiver).pipe(sender)
+
+  sender.request(msg, function (err, res) {
+    t.error(err, 'no error')
+    res.streams$.result.pipe(callback.obj(function (err, list) {
+      t.error(err, 'no error')
+      t.deepEqual(list, ['hello', 'streams'], 'is passed through correctly')
+    }))
+  })
+
+  receiver.on('request', function (req, reply) {
+    reply(null, {
+      streams$: {
+        result: from.obj(['hello', 'streams']).pipe(through.obj(function (chunk, enc, cb) {
+          console.log(chunk)
+          cb(null, chunk)
+        }))
       }
     })
   })
