38e6dcf6330a568036163efdcf24eee4a9898239
Matteo Collina <hello@matteocollina.com>
2015-08-04 20:44:55 +0200

    Added a setup step for the tests.

--- test.js
+++ test.js
@@ -7,22 +7,31 @@ var callback = require('callback-stream')
 var Writable = require('stream').Writable
 var through = require('through2')
 
-test('can issue a request', function (t) {
-  t.plan(3)
-
+function setup () {
   var sender = tentacoli()
   var receiver = tentacoli()
-  var msg = 'the answer to life, the universe and everything'
-  var expected = '42'
 
   sender.pipe(receiver).pipe(sender)
 
-  sender.request(msg, function (err, res) {
+  return {
+    sender: sender,
+    receiver: receiver
+  }
+}
+
+test('can issue a request', function (t) {
+  t.plan(3)
+
+  var s = setup()
+  var msg = 'the answer to life, the universe and everything'
+  var expected = '42'
+
+  s.sender.request(msg, function (err, res) {
     t.error(err, 'no error')
     t.deepEqual(res, expected, 'response matches')
   })
 
-  receiver.on('request', function (req, reply) {
+  s.receiver.on('request', function (req, reply) {
     t.deepEqual(req, msg, 'request matches')
     reply(null, expected)
   })
@@ -31,19 +40,16 @@ test('can issue a request', function (t) {
 test('can pass through an object', function (t) {
   t.plan(3)
 
-  var sender = tentacoli()
-  var receiver = tentacoli()
+  var s = setup()
   var msg = { cmd: 'the answer to life, the universe and everything' }
   var expected = { res: '42' }
 
-  sender.pipe(receiver).pipe(sender)
-
-  sender.request(msg, function (err, res) {
+  s.sender.request(msg, function (err, res) {
     t.error(err, 'no error')
     t.deepEqual(res, expected, 'response matches')
   })
 
-  receiver.on('request', function (req, reply) {
+  s.receiver.on('request', function (req, reply) {
     t.deepEqual(req, msg, 'request matches')
     reply(null, expected)
   })
@@ -52,13 +58,10 @@ test('can pass through an object', function (t) {
 test('can pass through object readable streams', function (t) {
   t.plan(3)
 
-  var sender = tentacoli()
-  var receiver = tentacoli()
+  var s = setup()
   var msg = { cmd: 'subscribe' }
 
-  sender.pipe(receiver).pipe(sender)
-
-  sender.request(msg, function (err, res) {
+  s.sender.request(msg, function (err, res) {
     t.error(err, 'no error')
     res.streams$.result.pipe(callback.obj(function (err, list) {
       t.error(err, 'no error')
@@ -66,7 +69,7 @@ test('can pass through object readable streams', function (t) {
     }))
   })
 
-  receiver.on('request', function (req, reply) {
+  s.receiver.on('request', function (req, reply) {
     reply(null, {
       streams$: {
         result: from.obj(['hello', 'streams'])
@@ -78,18 +81,15 @@ test('can pass through object readable streams', function (t) {
 test('can pass through object writable streams', function (t) {
   t.plan(2)
 
-  var sender = tentacoli()
-  var receiver = tentacoli()
+  var s = setup()
   var msg = { cmd: 'publish' }
 
-  sender.pipe(receiver).pipe(sender)
-
-  sender.request(null, function (err, res) {
+  s.sender.request(null, function (err, res) {
     t.error(err, 'no error')
     res.streams$.writable.end(msg)
   })
 
-  receiver.on('request', function (req, reply) {
+  s.receiver.on('request', function (req, reply) {
     var writable = new Writable({ objectMode: true })
     writable._write = function (chunk, enc, cb) {
       t.deepEqual(chunk, msg, 'msg match')
@@ -106,18 +106,15 @@ test('can pass through object writable streams', function (t) {
 test('can pass through a transform stream as a writable', function (t) {
   t.plan(2)
 
-  var sender = tentacoli()
-  var receiver = tentacoli()
+  var s = setup()
   var msg = { cmd: 'publish' }
 
-  sender.pipe(receiver).pipe(sender)
-
-  sender.request(null, function (err, res) {
+  s.sender.request(null, function (err, res) {
     t.error(err, 'no error')
     res.streams$.writable.end(msg)
   })
 
-  receiver.on('request', function (req, reply) {
+  s.receiver.on('request', function (req, reply) {
     var writable = new Writable({ objectMode: true })
     writable._write = function (chunk, enc, cb) {
       t.deepEqual(chunk, msg, 'msg match')
@@ -137,13 +134,10 @@ test('can pass through a transform stream as a writable', function (t) {
 test('can pass through a transform stream as a readable streams', function (t) {
   t.plan(3)
 
-  var sender = tentacoli()
-  var receiver = tentacoli()
+  var s = setup()
   var msg = { cmd: 'subscribe' }
 
-  sender.pipe(receiver).pipe(sender)
-
-  sender.request(msg, function (err, res) {
+  s.sender.request(msg, function (err, res) {
     t.error(err, 'no error')
     res.streams$.result.pipe(callback.obj(function (err, list) {
       t.error(err, 'no error')
@@ -151,7 +145,7 @@ test('can pass through a transform stream as a readable streams', function (t) {
     }))
   })
 
-  receiver.on('request', function (req, reply) {
+  s.receiver.on('request', function (req, reply) {
     reply(null, {
       streams$: {
         result: from.obj(['hello', 'streams']).pipe(through.obj(function (chunk, enc, cb) {
